71a41b1340a5e2e1737ea0728b36fdf6abcbfb84
tonyekas <tonyekas@yahoo.com>
2025-06-05 11:17:02 -0600

    making modifications

--- src/sum.ts
+++ src/sum.ts
@@ -3,3 +3,10 @@ function sum(a: number, b: number): number {
 }
 
 console.log('Sum of 3 and 5 is:', sum(3, 5));
+
+export default sum;
+// This function takes two numbers and returns their sum.
+
+function summation(a: number, b: number): number {
+  return a + b;
+}
